fd011409f88f4cf2ff2788afefeac0eccd17b55b
Atin Gupta <atingupta2005@gmail.com>
2025-11-29 14:28:36 +0530

    -

--- supabase/reset.sql
+++ supabase/reset.sql
@@ -26,4 +26,3 @@ REVOKE ALL ON FUNCTION public.exec_sql(text) FROM PUBLIC;
 REVOKE ALL ON FUNCTION public.exec_sql(text) FROM anon;
 REVOKE ALL ON FUNCTION public.exec_sql(text) FROM authenticated;
 GRANT EXECUTE ON FUNCTION public.exec_sql(text) TO service_role;
-GRANT EXECUTE ON FUNCTION public.exec_sql(text) TO admin_role;
